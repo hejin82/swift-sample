bf72af3defca5fe2239d230b703a3bb7ae338c71
hejin82 <hejin82@gmail.com>
2018-04-15 03:26:51 +0900

    init

---
+++ Notes/Notes/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Notes
+//
+//  Created by 何進 on 2018/4/15.
+//  Copyright © 2018年 何進. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
